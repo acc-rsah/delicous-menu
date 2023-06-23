b85bd2cff0c08b4156aa22fa496c5418ddbd3943
Rishav Sah <B0218404@airtel.com>
2023-06-24 02:20:29 +0530

    Community CSS

--- src/sections/Community/Communtiy.js
+++ src/sections/Community/Communtiy.js
@@ -3,6 +3,9 @@ import style from "./Community.module.css";
 import AppStrikedTitle from "../../components/AppStrikedTitle/AppStrikedTitle";
 import TextBlock from "../../components/TextBlock/TextBlock";
 import CommunityImgTxt from "../../components/CommunityImgTxt/CommunityImgTxt";
+import details from "../../assets/images/detail02.png";
+import det01 from "../../assets/images/detail01.png";
+import det03 from "../../assets/images/detail03.png";
 
 const menuJSON = [
     {
@@ -78,9 +81,9 @@ const Communtiy = () => {
     return (
         <section className={`${style.container} menu_section`} id="community">
             <div
-                className={`${style.imgNdTxtContainer} section ${
-                    isVisible[0] ? `${style.section_vis}` : ""
-                }`}
+                className={`${style.imgNdTxtContainer} ${
+                    style.row_Container_1
+                } section ${isVisible[0] ? `${style.section_vis}` : ""}`}
             >
                 <div className={style.title_container}>
                     <div>
@@ -95,33 +98,57 @@ const Communtiy = () => {
                         </p>
                     </div>
                 </div>
-                <CommunityImgTxt
-                    imagePath={menuJSON[0].picPath}
-                    altText={menuJSON[0].picName}
-                    title={menuJSON[0].cardDetail.title}
-                    bodyText={menuJSON[0].cardDetail.cardBodyText}
-                    commentCount={menuJSON[0].cardDetail.commentCount}
-                    heartCount={menuJSON[0].cardDetail.likeCount}
-                />
+                <div
+                    className={`${style.cardContainer} ${style.cardContainer_1}`}
+                >
+                    <img
+                        alt="details"
+                        src={details}
+                        className={style.cardContainer_1_det}
+                        loading="lazy"
+                    />
+                    <img
+                        alt="details01"
+                        src={det01}
+                        className={style.cardContainer_2_det}
+                        loading="lazy"
+                    />
+                    <CommunityImgTxt
+                        imagePath={menuJSON[0].picPath}
+                        altText={menuJSON[0].picName}
+                        title={menuJSON[0].cardDetail.title}
+                        bodyText={menuJSON[0].cardDetail.cardBodyText}
+                        commentCount={menuJSON[0].cardDetail.commentCount}
+                        heartCount={menuJSON[0].cardDetail.likeCount}
+                    />
+                </div>
             </div>
             <div
-                className={`${style.imgNdTxtContainer} section ${
-                    isVisible[1] ? `${style.section_vis}` : ""
-                }`}
+                className={`${style.imgNdTxtContainer} ${
+                    style.row_Container_2
+                } section ${isVisible[1] ? `${style.section_vis}` : ""}`}
             >
-                <CommunityImgTxt
-                    imagePath={menuJSON[1].picPath}
-                    altText={menuJSON[1].picName}
-                    title={menuJSON[1].cardDetail.title}
-                    bodyText={menuJSON[1].cardDetail.cardBodyText}
-                    commentCount={menuJSON[1].cardDetail.commentCount}
-                    heartCount={menuJSON[1].cardDetail.likeCount}
-                />
+                <div className={style.cardContainer}>
+                    <img
+                        alt="details"
+                        src={det03}
+                        className={style.cardContainer_3_det}
+                        loading="lazy"
+                    />
+                    <CommunityImgTxt
+                        imagePath={menuJSON[1].picPath}
+                        altText={menuJSON[1].picName}
+                        title={menuJSON[1].cardDetail.title}
+                        bodyText={menuJSON[1].cardDetail.cardBodyText}
+                        commentCount={menuJSON[1].cardDetail.commentCount}
+                        heartCount={menuJSON[1].cardDetail.likeCount}
+                    />
+                </div>
             </div>
             <div
                 className={`${style.imgNdTxtContainer} section ${
-                    isVisible[2] ? `${style.section_vis}` : ""
-                }`}
+                    style.row_Container_3
+                } ${isVisible[2] ? `${style.section_vis}` : ""}`}
             >
                 <div className={style.sideText}>
                     <p>
@@ -130,14 +157,16 @@ const Communtiy = () => {
                         magna aliqua. Duis aute irure.
                     </p>
                 </div>
-                <CommunityImgTxt
-                    imagePath={menuJSON[2].picPath}
-                    altText={menuJSON[2].picName}
-                    title={menuJSON[2].cardDetail.title}
-                    bodyText={menuJSON[2].cardDetail.cardBodyText}
-                    commentCount={menuJSON[2].cardDetail.commentCount}
-                    heartCount={menuJSON[2].cardDetail.likeCount}
-                />
+                <div className={`${style.cardContainer} ${style.lastRow_card}`}>
+                    <CommunityImgTxt
+                        imagePath={menuJSON[2].picPath}
+                        altText={menuJSON[2].picName}
+                        title={menuJSON[2].cardDetail.title}
+                        bodyText={menuJSON[2].cardDetail.cardBodyText}
+                        commentCount={menuJSON[2].cardDetail.commentCount}
+                        heartCount={menuJSON[2].cardDetail.likeCount}
+                    />
+                </div>
             </div>
         </section>
     );
