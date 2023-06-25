9ef704c6c5bcbf1adf240e61af93faf1cca80b8f
Rishav Sah <B0218404@airtel.com>
2023-06-26 00:11:33 +0530

    Popular Recipes

--- src/sections/PopularRecipes/PopularRecipes.js
+++ src/sections/PopularRecipes/PopularRecipes.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./PopularRecipes.module.css";
 import AppStrikedTitle from "../../components/AppStrikedTitle/AppStrikedTitle";
 import TextBlock from "../../components/TextBlock/TextBlock";
 import RecipeImgTxt from "../../components/RecipeImgTxt/RecipeImgTxt";
+import BackBtn from "../../assets/images/back.png";
+import NextBtn from "../../assets/images/next.png";
 
 const PopularRecipes = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const containerRef = useRef(null);
+
+    const scrollToIndex = (index) => {
+        if (containerRef.current) {
+            containerRef.current.scrollLeft =
+                index * containerRef.current.offsetWidth;
+        }
+        setCurrentIndex(index);
+    };
+
+    useEffect(() => {
+        let dishTitle = document.querySelector("#dish_title");
+        let leftPosition = dishTitle.getBoundingClientRect()?.left;
+        let dishSection = document.querySelector("#dish_section");
+        dishSection.style.paddingLeft = leftPosition + "px";
+    }, []);
+
     const popRecipes = [
         {
             picName: "Sandwich",
@@ -111,7 +131,50 @@ const PopularRecipes = () => {
                     "Grab just four ingredients to make this easy, light lunch and enjoy a taste of summer. The combination of peaches, feta alongside of vegies make it so yumm",
             },
         },
+        {
+            picName: "PineApple",
+            picPath: "pineapple.png",
+            serving: "2",
+            chefChoice: false,
+            cardSize: "small",
+            chefName: "User Full Name",
+            cardPosition: "top",
+            cardDetail: {
+                level: "Difficulty",
+                dishName: "PINEAPPLE FRUIT SALAD WITH FRESH CREAM",
+                dishDesc:
+                    "The pineapple juice is the star of the dressing and what gives it such a refreshing and sweet taste. Pineapple juice is known for its health benefit",
+            },
+        },
+        {
+            picName: "CheeseSpinach",
+            picPath: "CheeseSpinach.png",
+            serving: "2",
+            chefChoice: true,
+            cardSize: "small",
+            chefName: "User Full Name",
+            cardPosition: "bottom",
+            cardDetail: {
+                level: "Difficulty",
+                dishName: "CHEESE, SPINACH & MUSHROOM STUFFED CHILLY SERVINGS",
+                dishDesc:
+                    "Amp up this dinner party classic by stuffing three types of cheese, spinach and mushroom into the chicken and flavoured along side with chicken soup",
+            },
+        },
     ];
+
+    const scrollToPrevious = () => {
+        if (currentIndex > 0) {
+            scrollToIndex(currentIndex - 1);
+        }
+    };
+
+    const scrollToNext = () => {
+        if (currentIndex < popRecipes.length - 1) {
+            scrollToIndex(currentIndex + 1);
+        }
+    };
+
     return (
         <section
             className={`${style.container} menu_section`}
@@ -131,10 +194,42 @@ const PopularRecipes = () => {
                 </div>
             </div>
 
-            <div className={style.dish_section} id="dish_section">
+            <div
+                className={style.dish_section}
+                id="dish_section"
+                ref={containerRef}
+            >
                 {popRecipes.map((recipe, index) => (
                     <RecipeImgTxt recipe={recipe} key={index} />
                 ))}
+            </div>
+            <div className={style.prevNextBtn} id="prevNextBtn">
+                <img
+                    alt="Back Button"
+                    src={currentIndex === 0 ? BackBtn : NextBtn}
+                    loading="lazy"
+                    onClick={scrollToPrevious}
+                    className={
+                        currentIndex === 0
+                            ? `${style.inactive}`
+                            : `${style.imgPrevBtn} ${style.active}`
+                    }
+                />
+                <img
+                    alt="Next Button"
+                    src={
+                        currentIndex === popRecipes.length - 1
+                            ? BackBtn
+                            : NextBtn
+                    }
+                    loading="lazy"
+                    onClick={scrollToNext}
+                    className={
+                        currentIndex === popRecipes.length - 1
+                            ? `${style.imgNextBtn} ${style.inactive} `
+                            : `${style.active}`
+                    }
+                />
             </div>
         </section>
     );
