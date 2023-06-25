19223b2c786f85f2759f0c3d82791fb0cc85a9df
Rishav Sah <B0218404@airtel.com>
2023-06-25 20:43:17 +0530

    Our Menu Section

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
             <Communtiy />
             <Map />
             <Menu data={data} />
-            {/*  <PopularRecipes /> */}
+            <PopularRecipes />
             <Collaborate />
             <Footer />
         </div>
--- src/sections/Menu/Menu.js
+++ src/sections/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Menu.module.css";
 import AppStrikedTitle from "../../components/AppStrikedTitle/AppStrikedTitle";
 import TextBlock from "../../components/TextBlock/TextBlock";
@@ -15,13 +15,93 @@ const Menu = ({ data }) => {
         menuByFoodType[type].push(rest);
     }
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const menuSection = document.querySelector("#dish_menu_section");
+            menuSection.style.opacity = 1;
+            const column1 = document.querySelector(
+                "#foodCatContainer > div:nth-child(1) > div"
+            );
+
+            const column2 = document.querySelector(
+                "#foodCatContainer > div:nth-child(2) > div"
+            );
+            const column3 = document.querySelector(
+                "#foodCatContainer > div:nth-child(3) > div"
+            );
+            const column4 = document.querySelector(
+                "#foodCatContainer > div:nth-child(4) > div"
+            );
+            if (menuSection) {
+                const scrollTop =
+                    window.pageYOffset || document.documentElement.scrollTop;
+                const sectionTop = menuSection.offsetTop;
+                const sectionHeight = menuSection.offsetHeight;
+                const windowHeight = window.innerHeight;
+                const cHeight = menuSection.clientHeight;
+
+                const scrollBottom = scrollTop + windowHeight;
+
+                const visibleHeight = Math.max(
+                    0,
+                    Math.min(
+                        windowHeight,
+                        sectionTop + sectionHeight - scrollTop,
+                        scrollTop + windowHeight - sectionTop
+                    )
+                );
+
+                if (visibleHeight <= 50) {
+                    menuSection.style.opacity = 0;
+                }
+
+                let percentage = (visibleHeight / sectionHeight) * 100;
+                if (scrollBottom >= sectionTop + cHeight) {
+                    if (percentage) {
+                        column2.style.transform = `translateY(${
+                            100 - percentage
+                        }%)`;
+                        column4.style.transform = `translateY(${
+                            100 - percentage
+                        }%)`;
+                        column1.style.transform = `translateY(-${
+                            100 - percentage
+                        }%)`;
+                        column3.style.transform = `translateY(-${
+                            100 - percentage
+                        }%)`;
+                    }
+                } else {
+                    column2.style.transform = `translateY(-${
+                        100 - percentage
+                    }%)`;
+                    column4.style.transform = `translateY(-${
+                        100 - percentage
+                    }%)`;
+                    column1.style.transform = `translateY(${
+                        100 - percentage
+                    }%)`;
+                    column3.style.transform = `translateY(${
+                        100 - percentage
+                    }%)`;
+                }
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     const renderMenu = (menuType) => {
         return (
             <div className={styles.foodCardContainer}>
-                <MenuTitleNdFoodCard
-                    menuType={menuType}
-                    menuByFoodType={menuByFoodType}
-                />
+                <div className={styles.foodInnerContainer}>
+                    <MenuTitleNdFoodCard
+                        menuType={menuType}
+                        menuByFoodType={menuByFoodType}
+                    />
+                </div>
             </div>
         );
     };
@@ -35,7 +115,7 @@ const Menu = ({ data }) => {
                 <AppStrikedTitle text="Our Menu" variant="variant_1" />
                 <TextBlock text="Know More" variant="variant_1" />
             </div>
-            <div className={styles.foodTypeContainer}>
+            <div className={styles.foodTypeContainer} id="foodCatContainer">
                 {Object.keys(menuByFoodType).map((ele, index) => {
                     //et menuType = ele.toLowerCase().split("_").join(" ");
                     let menuType = ele.toLowerCase();
